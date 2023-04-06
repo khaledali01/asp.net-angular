789e8ded620e7eb4ee8dd107bc253b66b7bcc330
Khaled <khaled.ali.architect@gmail>
2023-04-07 00:06:11 +0200

    Refactoring Angular app
    See: #4

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
