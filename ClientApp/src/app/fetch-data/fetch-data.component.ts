68bd2142a9f0cafa3d57cb6340591bc6fe17b3e8
Khaled <khaled.ali.architect@gmail>
2023-04-06 17:51:29 +0200

    Changing the API Endpoints
    See: #1

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -8,8 +8,8 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public forecasts: WeatherForecast[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
